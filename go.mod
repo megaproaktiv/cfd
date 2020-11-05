0b2ef0f3d5822ae345cd169e3a49f05b446a502c
Gernot Glawe <gglawe@tecracer.de>
2020-11-05 18:22:51 +0100

    First steps

---
+++ go.mod
@@ -0,0 +1,9 @@
+module cloudformationdeploy
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v0.2.2
+	github.com/aws/aws-sdk-go-v2/service/cloudformation v0.29.0
+	github.com/awslabs/goformation/v4 v4.15.4
+)
---
+++ main/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	// "fmt"
+	"strconv"
+	"time"
+	"cloudformationdeploy"
+	"github.com/aws/aws-sdk-go-v2/config"
+	cfnservice "github.com/aws/aws-sdk-go-v2/service/cloudformation"
+	"github.com/awslabs/goformation/v4/cloudformation"
+	"github.com/awslabs/goformation/v4/cloudformation/sns"
+
+
+)
+
+func main() {
+
+	// Create a new CloudFormation template
+	template := cloudformation.NewTemplate()
+
+	// Create an Amazon SNS topic, with a unique name based off the current timestamp
+	template.Resources["MyTopic"] = &sns.Topic{
+		TopicName: "my-topic-" + strconv.FormatInt(time.Now().Unix(), 10),
+	}
+
+	template.Resources["NotMyTopic"] = &sns.Topic{
+		TopicName: "my-topic2-" + strconv.FormatInt(time.Now().Unix(), 10),
+	}
+
+	//fmt.Print(len(template.Resources))
+
+
+	cfg, err := config.LoadDefaultConfig(config.WithRegion("eu-central-1"))
+    if err != nil {
+        panic("unable to load SDK config, " + err.Error())
+	}	
+	client := cfnservice.NewFromConfig(cfg);
+
+	const stackname = "testcfn"
+	cloudformationdeploy.CreateStack(client,stackname, template)
+
+	cloudformationdeploy.ShowStatus(client,stackname);
+}
