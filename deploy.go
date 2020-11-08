ce4814787c18e8464fd0937aad36b822079967ea
Gernot Glawe <gglawe@tecracer.de>
2020-11-08 11:43:25 +0100

    Test 1 OK - populate data

--- deploy.go
+++ deploy.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	cfn "github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"fmt"
-	// "github.com/jroimartin/gocui"
 	"github.com/awslabs/goformation/v4/cloudformation"
 	"time"
+	"github.com/alexeyco/simpletable"
+	// tm "github.com/buger/goterm"
+
 )
 
 // CloudFormationResource holder for status
 type CloudFormationResource struct {
-	Name string
-	Type string
+	LogicalResourceId string
+	PhysicalResourceId string
 	Status string
+	Type string
+	Timestamp time.Time
 }
 
 //go:generate moq -out deploy_moq_test.go . DeployInterface
@@ -52,13 +56,80 @@ func DeleteStack(client DeployInterface,name string){
 
 
 // ShowStatus status of stack
-func ShowStatus(client DeployInterface, name string){
+func ShowStatus(client DeployInterface, name string, template *cloudformation.Template){
 	
-	doEvery(2000*time.Millisecond,client,name)
+	//maxRows := len(template.Resources)+1;
+	
+    data := map[string]CloudFormationResource{}
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: "#"},
+			{Align: simpletable.AlignLeft, Text: "STATE"},
+			{Align: simpletable.AlignLeft, Text: "Logical"},
+			{Align: simpletable.AlignLeft, Text: "Type"},
+		},
+		
+	}
+
+
+	i := 1
+	for k, v := range template.Resources {
+		r := []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: fmt.Sprintf("%d", i)},
+			{Align: simpletable.AlignLeft, Text: "state"},
+			{Align: simpletable.AlignLeft, Text: k},
+			{Align: simpletable.AlignLeft, Text: v.AWSCloudFormationType()},
+		}
+		table.Body.Cells = append(table.Body.Cells, r)
+		i = i+1
+		item := &CloudFormationResource{
+			LogicalResourceId: k,
+			PhysicalResourceId: "",
+			Status: "-",
+			Type: v.AWSCloudFormationType(),
+		}
+		data[k] = *item;
+	}
+	
+	// table.SetStyle(simpletable.StyleCompactClassic)
+	// tm.Clear()
+	// tm.MoveCursor(1,1)
+
+	// tm.Println(table.String())
+	// tm.Flush()
+
+	i = 0;
+	for id, v := range template.Resources {
+		r := []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: fmt.Sprintf("%d", i)},
+			{Align: simpletable.AlignLeft, Text: data[id].Status},
+			{Align: simpletable.AlignLeft, Text: id},
+			{Align: simpletable.AlignLeft, Text: v.AWSCloudFormationType()},
+		}
+		table.Body.Cells = append(table.Body.Cells, r)
+		i = i+1;
+	}
+	// tm.Clear()
+	// tm.MoveCursor(1,1)
+	// tm.Println(table.String())
+	// tm.Flush()
+	
+	doEvery(2000*time.Millisecond,client,name,template, table,data)
 	
 }
 
-func showSingleStatus(client DeployInterface, name string){
+func doEvery(d time.Duration, client DeployInterface, name string,template  *cloudformation.Template, table *simpletable.Table, data map[string]CloudFormationResource) {
+	for range time.Tick(d) {
+		PopulateData(client, name, data)
+	}
+
+}
+
+// PopulateData update status from describe call
+func PopulateData(client DeployInterface, name string,data map[string]CloudFormationResource)( map[string]CloudFormationResource){
 	params := &cfn.DescribeStackEventsInput{
 		StackName: &name,
 	}
@@ -67,18 +138,20 @@ func showSingleStatus(client DeployInterface, name string){
 		panic(error)
 	}
 
+	// Update Status and Timestamp if newer
 	for i := 0; i < len(output.StackEvents); i++ {
-		event := *output.StackEvents[i];
 		
-		fmt.Println(*event.LogicalResourceId, " : ", event.ResourceStatus, ": ", *event.ResourceType);
+		event := *output.StackEvents[i];		
+		item := data[*event.LogicalResourceId]
+
+		if( event.Timestamp.After(item.Timestamp) ){
+			item.Status = string(event.ResourceStatus);
+			item.Timestamp = *event.Timestamp;
+			data[*event.LogicalResourceId] = item;
+			
+		}
 		
 	}
-	fmt.Println("==========");
+	return data;
 
-}
-
-func doEvery(d time.Duration, client DeployInterface, name string) {
-	for range time.Tick(d) {
-		showSingleStatus(client, name)
-	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module cloudformationdeploy
 go 1.15
 
 require (
+	github.com/alexeyco/simpletable v0.0.0-20200730140406-5bb24159ccfb
 	github.com/aws/aws-sdk-go-v2/config v0.2.2
 	github.com/aws/aws-sdk-go-v2/service/cloudformation v0.29.0
 	github.com/awslabs/goformation/v4 v4.15.4
+	github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gotest.tools v2.2.0+incompatible
 )
